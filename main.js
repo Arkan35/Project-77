f6d7c84072a56d5e20db3c71fea59c3fd73b8110
Arkan35 <82638961+Arkan35@users.noreply.github.com>
2022-01-17 15:32:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a = 10;
+var b = 5;
+var add = a + b ;
+console.log(add);
+var a = 3;
+var b = 2;
+var add = a + b ;
+console.log(add)
+var a = 25;
+var b = 2;
+var multiply = a * b ;
+console.log(multiply)
+var a = 1;
+var b = 1;
+var add = a + b ;
+console.log(add)
